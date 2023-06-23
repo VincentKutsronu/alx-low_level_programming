13140429d8a648ef6acc72f936fbbc0b4a0989c7
VincentKutsronu <vincentkuts@gmail.com>
2023-06-23 16:18:43 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,10 @@
  * Return: 0 or 1
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
-
